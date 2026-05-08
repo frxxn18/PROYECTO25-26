ac1b802b6f350eaea60af9126406d96b7e9e58d0
Ivanchuu18 <ivantxulogl@gmail.com>
2026-05-08 15:34:57 +0200

    lo_mismo_para_el_login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <link rel="icon" href="{{ asset('assets/img/logo.png') }}" type="image/png">
 </head>
 <body class="bg-light">
 
