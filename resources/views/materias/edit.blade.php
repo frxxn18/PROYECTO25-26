0e84295d23e9e00573bb696c0a95e699c4827fcc
frxxn18 <franpado8@gmail.com>
2026-05-19 13:11:31 +0200

    fallo en la vista materia/edit arreglado

--- resources/views/materias/edit.blade.php
+++ resources/views/materias/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('titulo', 'Editar materia')
 
-@section('content")
+@section('content')
 <div class="row justify-content-center">
     <div class="col-md-4">
         <div class="card shadow-sm">
