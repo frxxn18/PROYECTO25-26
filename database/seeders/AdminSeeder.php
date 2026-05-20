49f0fff9db9120c245249d7207b10458690be5fa
frxxn18 <franpado8@gmail.com>
2026-05-20 13:42:52 +0200

    Correccion de rol de administrador

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -14,6 +14,7 @@ class AdminSeeder extends Seeder
             'name'     => 'Administrador',
             'email'    => 'admin@admin.com',
             'password' => Hash::make('admin1234'),
+            'role'     => 'admin',
         ]);
     }
 }
